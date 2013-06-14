9ffe03769166a375300238f58d91325793ac9c8d
ajfaraday <andrewfaraday@hotmail.co.uk>
2013-06-14 22:59:36 +0100

    nicer jquery animation

--- static/javascript/teabot.js
+++ static/javascript/teabot.js
@@ -1,31 +1,51 @@
-function set_fill(percent){
-  if (percent > 100)
-  {
-    percent = 100 ;
+function set_fill(percent) {
+  //jQuery('#percent_display').html(percent);
+  jQuery('#percent_display').removeClass('red');
+  if (percent > 100) {
+    jQuery('#percent_display').addClass('red');
+    percent = 100;
   }
-  if (percent < 0)
-  {
-    percent = 0 ;
+  if (percent < 0) {
+    percent = 0;
+  }
+  if (percent < 10) {
+    jQuery('#percent_display').addClass('red');
   }
   var tea = jQuery('#tea');
   var height = 275.0;
   var offset = 95.0;
 
-  var tea_height = (percent/100.0)*height;
-  var tea_top = ((height-tea_height+offset));
+  var tea_height = (percent / 100.0) * height;
+  var tea_top = ((height - tea_height + offset));
 
   //Plain non-animated method of setting height
   //tea.css('height',tea_height);
   //tea.css('top',tea_top);
 
-  tea.animate({'height':tea_height,'top':tea_top},1000);
-  jQuery('#percent_display').html(percent);
+  tea.animate({'height':tea_height, 'top':tea_top},{duration: 1000});
+  jQuery({value:$('#percent_display').html()}).animate(
+    {value:percent},
+    {
+      duration: 1000,
+      step: function(){
+        $('#percent_display').text(Math.ceil(this.value));
+      }
+    }
+  );
 }
 
-function start_polling(){
-    var $tide = setInterval(function(){
-        set_fill(90);
-        set_fill(20)
-    },10000);
+function start_polling() {
+  var $tide = setInterval(function () {
+    set_fill(90);
+    set_fill(20);
+  }, 10000);
 }
+
+function stop_polling(){
+  window.clearInterval($tide);
+}
+
+
+
+
 
